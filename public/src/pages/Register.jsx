e92f69ef9828fbbd816c7dd08c5fd152ba2a4d2d
Onkar-1-2-3 <onkar.mishra.me@ghrce.raisoni.net>
2024-02-21 16:27:57 +0530

    added Register Functionalities using react toastify

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import styled from "styled-components";
 import Logo from "../assets/logo.svg";
+import {ToastContainer, toast} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
 
 function Register() {
+    const[values, setValues] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
+
+    const toastOptions={
+        position: "bottom-right",
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "dark",
+    };
     const handleSubmit = (event) =>{
         event.preventDefault();
-        alert("form");
+        handleValidation();
     };
 
-    const handleChange = (event) => {};
+    const handleValidation = () => {
+        const { password, confirmPassword, username, email } = values;
+        if (password !== confirmPassword) {
+            toast.error("password and confirm password should be same", toastOptions );
+            return false;
+        } else if( username.length < 3){
+            toast.error("Username should be greater than 3 characters", toastOptions );
+            return false;
+        } else if( password.length < 8){
+            toast.error("Password should be greater than 8 characters", toastOptions );
+            return false;
+        } else if ( email === ""){
+            toast.error("Email is required", toastOptions);
+            return false;
+        }
+        return true;
+    };
+
+    const handleChange = (event) => {
+        setValues({ ...values, [event.target.name]: event.target.value });
+    };
 
     return( 
     <>
@@ -27,7 +63,9 @@ function Register() {
                 <span>Already have an account? <Link to="/login">Login</Link></span>
             </form>
         </FormContainer>
-    </> )
+        <ToastContainer />
+    </> 
+    );
 }
 
 const FormContainer = styled.div`
